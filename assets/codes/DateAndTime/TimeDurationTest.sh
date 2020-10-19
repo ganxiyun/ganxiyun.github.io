fcc1ff3281b4dc0de4124d5c53ecf96153dc53fe
ganxiyun <ganxiyun@gmail.com>
2020-10-19 23:06:30 +0800

    Create TimeDurationTest.sh

---
+++ assets/codes/DateAndTime/TimeDurationTest.sh
@@ -0,0 +1,21 @@
+#/bin/bash -ex
+
+# MAKE SURE the NTP is turned off before running the script, otherwise the Date Time of the system might be synchornized during this experiment. 
+
+# replace the classpath with the correct ones
+java -Dfile.encoding=UTF-8 -classpath ~/Time:~/.m2/repository/com/google/guava/guava/18.0/guava-18.0.jar TimeDurationTest &
+java_pid=$!
+
+sleep 1
+
+now=`date +%s` #epoch in milliseconds
+
+newTime=`expr $now - 10`
+
+echo "time before setting" `date`
+
+sudo date -s @$newTime
+
+echo "time after setting" `date`
+
+wait $java_pid
