29cd9a248e321db6d266482f0448a6ac8c3eee56
ganxiyun <ganxiyun@gmail.com>
2020-10-20 22:40:54 +0800

    add example to stop ntp

--- assets/codes/DateAndTime/TimeDurationTest.sh
+++ assets/codes/DateAndTime/TimeDurationTest.sh
@@ -1,7 +1,8 @@
 #/bin/bash -ex
 
 # MAKE SURE the NTP is turned off before running the script, otherwise the Date Time of the system might be synchornized during this experiment. 
-
+# For example, in Ubuntu 20.04, 
+# `systemctl stop systemd-timesyncd`
 # replace the classpath with the correct ones
 java -Dfile.encoding=UTF-8 -classpath ~/Time:~/.m2/repository/com/google/guava/guava/18.0/guava-18.0.jar TimeDurationTest &
 java_pid=$!
